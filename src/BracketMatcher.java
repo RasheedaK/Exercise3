32379e2322bc3a7a6d799e333e1855b232658db9
krashee <krashee@thoughtworks.com>
2017-02-12 12:46:41 +0530

    Add only braces from the input String to buffer and test a valid math expression

--- src/BracketMatcher.java
+++ src/BracketMatcher.java
@@ -5,19 +5,19 @@ import java.util.Stack;
 
 class BracketMatcher {
     private final Stack stack;
-    private final Map<Character,Character> bracketMapping;
-    BracketMatcher(Stack stack, Map<Character,Character> brackets) {
+    private final Map<Character, Character> bracketMapping;
+
+    BracketMatcher(Stack stack, Map<Character, Character> brackets) {
         this.stack = stack;
         this.bracketMapping = brackets;
     }
 
     boolean areBracketsMatched(String inputString) {
-        List<Character> brackets=normalizeInput(inputString);
-        for (Character bracket:brackets) {
-            if(bracketMapping.containsValue(bracket)){
+        List<Character> brackets = normalizeInput(inputString);
+        for (Character bracket : brackets) {
+            if (bracketMapping.containsValue(bracket)) {
                 stack.push(bracket);
-            }
-            else {
+            } else {
                 return testMatching(bracket);
             }
         }
@@ -25,38 +25,38 @@ class BracketMatcher {
     }
 
     private List<Character> normalizeInput(String inputString) {
-        String input=removeNonBraces(inputString);
-        List<Character> brackets=new ArrayList<>();
-        for(int i=0;i<input.length();i++){
+        String input = removeNonBraces(inputString);
+        List<Character> brackets = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
             brackets.add(input.charAt(i));
         }
         return brackets;
     }
 
     private String removeNonBraces(String inputString) {
-        StringBuffer stringBuffer=new StringBuffer(inputString);
-        for(int i=0;i<stringBuffer.length();i++){
-            if(!bracketMapping.containsValue(inputString.charAt(i)) && !bracketMapping.containsKey(inputString.charAt(i))){
-                stringBuffer.deleteCharAt(i);
+        System.out.println(inputString);
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < inputString.length(); i++) {
+            if (bracketMapping.containsValue(inputString.charAt(i)) || bracketMapping.containsKey(inputString.charAt(i))) {
+                stringBuffer.append(inputString.charAt(i));
             }
         }
-      return new String(stringBuffer);
+        return new String(stringBuffer);
     }
 
     private boolean testMatching(Character bracket) {
-        if(!isStackEmpty()) {
+        if (!isStackEmpty()) {
             Character poppedBracket = (Character) stack.pop();
             if (bracketMapping.get(bracket).equals(poppedBracket)) {
                 return true;
             } else
                 return false;
-        }
-        else
+        } else
             return false;
     }
 
     private boolean isStackEmpty() {
-        if(stack.empty())
+        if (stack.empty())
             return true;
         else
             return false;
--- test/BracketMatcherTest.java
+++ test/BracketMatcherTest.java
@@ -8,52 +8,63 @@ import static org.junit.Assert.*;
 public class BracketMatcherTest {
     @Test
     public void returnTrueForPairedSquareBrackets() {
-        Map<Character,Character> bracketMapping = getBracketMapping();
+        Map<Character, Character> bracketMapping = getBracketMapping();
         Stack stack = new Stack();
-        BracketMatcher bracketMatcher = new BracketMatcher(stack,bracketMapping);
-        String input="[]";
+        BracketMatcher bracketMatcher = new BracketMatcher(stack, bracketMapping);
+        String input = "[]";
         assertTrue(bracketMatcher.areBracketsMatched(input));
     }
+
     @Test
     public void returnFalseForUnPairedBrackets() {
-        Map<Character,Character> bracketMapping = getBracketMapping();
+        Map<Character, Character> bracketMapping = getBracketMapping();
         Stack stack = new Stack();
-        BracketMatcher bracketMatcher = new BracketMatcher(stack,bracketMapping);
-        String input="[[";
+        BracketMatcher bracketMatcher = new BracketMatcher(stack, bracketMapping);
+        String input = "[[";
         assertFalse(bracketMatcher.areBracketsMatched(input));
     }
+
     @Test
     public void returnFalseForUnOrderedBrackets() {
-        Map<Character,Character> bracketMapping = getBracketMapping();
+        Map<Character, Character> bracketMapping = getBracketMapping();
         Stack stack = new Stack();
-        BracketMatcher bracketMatcher = new BracketMatcher(stack,bracketMapping);
-        String input="}{";
+        BracketMatcher bracketMatcher = new BracketMatcher(stack, bracketMapping);
+        String input = "}{";
         assertFalse(bracketMatcher.areBracketsMatched(input));
     }
 
     @Test
     public void returnTrueForPairedAndNestedBrackets() {
-        Map<Character,Character> bracketMapping = getBracketMapping();
-        String input="}{";
+        Map<Character, Character> bracketMapping = getBracketMapping();
+        String input = "}{";
         Stack stack = new Stack();
-        BracketMatcher bracketMatcher = new BracketMatcher(stack,bracketMapping);
+        BracketMatcher bracketMatcher = new BracketMatcher(stack, bracketMapping);
         assertFalse(bracketMatcher.areBracketsMatched(input));
     }
 
     @Test
     public void returnFalseForPairedAndIncorrectlyNestedBrackets() {
-        Map<Character,Character> bracketMapping = getBracketMapping();
-        String input="[({]})";
+        Map<Character, Character> bracketMapping = getBracketMapping();
+        String input = "[({]})";
         Stack stack = new Stack();
-        BracketMatcher bracketMatcher = new BracketMatcher(stack,bracketMapping);
+        BracketMatcher bracketMatcher = new BracketMatcher(stack, bracketMapping);
         assertFalse(bracketMatcher.areBracketsMatched(input));
     }
 
-    private Map<Character,Character> getBracketMapping() {
-        Map<Character,Character> brackets = new HashMap<>();
-        brackets.put('}','{');
-        brackets.put(']','[');
-        brackets.put(')','(');
+    @Test
+    public void returnTrueForValidMathExpression() {
+        Map<Character, Character> bracketMapping = getBracketMapping();
+        String input = "(((185+223.85)*15)-543)/2";
+        Stack stack = new Stack();
+        BracketMatcher bracketMatcher = new BracketMatcher(stack, bracketMapping);
+        assertTrue(bracketMatcher.areBracketsMatched(input));
+    }
+
+    private Map<Character, Character> getBracketMapping() {
+        Map<Character, Character> brackets = new HashMap<>();
+        brackets.put('}', '{');
+        brackets.put(']', '[');
+        brackets.put(')', '(');
         return brackets;
     }
 }
